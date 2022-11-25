0da4a6a2a80a838a1776a4ad5abdd7ba4122a54e
jehadalhorany <alhoranyjehad@gmail.com>
2022-11-25 15:39:11 +0300

    add app seleter

--- anycehicle-front/src/App.js
+++ anycehicle-front/src/App.js
@@ -7,7 +7,7 @@ import Maintenance from './components/Maintenance';
 function App() {
   return (
     <div className="App">
-      {/* <NavBar/> */}
+      <NavBar/>
       <Routes>
         <Route path={"/register"} element={<Register />} />
         <Route path="/login" element={<Login />} />
