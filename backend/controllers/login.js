df396c9d201abbd668c8b1734603b59c4728b8b8
jehadalhorany <alhoranyjehad@gmail.com>
2022-11-25 15:41:26 +0300

    login Eidt and test it

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -2,7 +2,6 @@ const connection = require("../models/db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
 const login = (req, res) => {
     const { password } = req.body
     const email = req.body.email.toLowerCase();
@@ -40,7 +39,6 @@ const login = (req, res) => {
         }
     });
 };
-
 
 module.exports = {
     login,
