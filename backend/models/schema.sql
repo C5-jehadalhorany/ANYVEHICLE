3e36f86b9704a0076b4b08ee01fe6294fb55e843
jehadalhorany <alhoranyjehad@gmail.com>
2022-11-27 01:36:31 +0300

    Eidt finshing

--- backend/models/schema.sql
+++ backend/models/schema.sql
@@ -33,21 +33,3 @@ CREATE TABLE maintenance(
     is_deleted TINYINT DEFAULT 0,
     PRIMARY KEY (id)
 );
-
-
-CREATE TABLE permissions (
-    id INT AUTO_INCREMENT NOT NULL,
-    permission VARCHAR(255) NOT NULL,
-    is_deleted TINYINT DEFAULT 0,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE role_permission (
-    id INT NOT NULL AUTO_INCREMENT NOT NULL,
-    role_id INT,
-    permission_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles (id),
-    FOREIGN KEY (permission_id) REFERENCES permissions (id),
-    is_deleted TINYINT DEFAULT 0,
-    PRIMARY KEY (id)
-);
